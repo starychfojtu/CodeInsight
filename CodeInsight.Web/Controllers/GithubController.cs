eb0a210824eede209a27304ad8cc2f8733ff3f8c
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2019-03-09 23:07:14 +0100

    Controller refactoring

--- CodeInsight.Web/Controllers/GithubController.cs
+++ CodeInsight.Web/Controllers/GithubController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CodeInsight.Github;
-using CodeInsight.Library;
 using CodeInsight.Library.Extensions;
 using CodeInsight.Library.Types;
 using CodeInsight.Web.Common;
@@ -12,7 +11,6 @@ using CodeInsight.Web.Models.Github;
 using FuncSharp;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Monad;
 using Octokit;
 using Octokit.GraphQL;
 using Controller = Microsoft.AspNetCore.Mvc.Controller;
@@ -35,15 +33,28 @@ namespace CodeInsight.Web.Controllers
             this.client = new GitHubClient(new ProductHeaderValue(configuration.ApplicationName));
         }
 
-        public IActionResult SignIn()
+        #region SignIn
+
+        public IActionResult SignIn() =>
+            CreateOAuthLoginUrl()
+                .ToString()
+                .Pipe(Redirect);
+        
+        private Uri CreateOAuthLoginUrl()
         {
             var csrfToken = Guid.NewGuid().ToString();
-            var oauthLoginUrl = GetOAuthLoginUrl(csrfToken);
-            
             HttpContext.Session.Set(CsrfSessionKey, csrfToken);
-
-            return Redirect(oauthLoginUrl.ToString());
+            
+            return client.Oauth.GetGitHubLoginUrl(new OauthLoginRequest(configuration.ClientId)
+            {
+                Scopes = { "read:org", "repo" },
+                State = csrfToken
+            });
         }
+        
+        #endregion
+        
+        #region Authorize
 
         public Task<IActionResult> Authorize(string code, string state)
         {
@@ -60,15 +71,33 @@ namespace CodeInsight.Web.Controllers
                 _ => RedirectToAction("Index", "Home").Async<RedirectToActionResult, IActionResult>()
             );
         }
+        
+        private static IOption<NonEmptyString> GetVerifiedCode(string code, string state, ISession session)
+        {
+            var expectedState = session.Get<string>(CsrfSessionKey).GetOrElse("");
+            return NonEmptyString.Create(code).Where(_ => state == expectedState);
+        }
+        
+        private Task<string> GetOAuthAccessToken(NonEmptyString code)
+        {
+            var request = new OauthTokenRequest(configuration.ClientId, configuration.ClientSecret, code);
+            return client.Oauth.CreateAccessToken(request).Map(t => t.AccessToken);
+        }
+        
+        #endregion
+
+        #region ChooseRepository
+
+        
+
+        
 
         [HttpGet]
-        public Task<IActionResult> ChooseRepository() => ConnectionAction(conn =>
-        {
-            return conn
+        public Task<IActionResult> ChooseRepository() => ConnectionAction(connection =>
+            connection
                 .Run(GetAllRepositoriesQuery())
                 .Map(items => new ChooseRepositoryViewModel(items.SelectMany(i => i)))
-                .Map(vm => (IActionResult) View(vm));
-        });
+                .Map(vm => (IActionResult) View(vm)));
 
         [HttpPost]
         public Task<IActionResult> ChooseRepository(string nameWithOwner) => ConnectionAction(async conn =>
@@ -92,36 +121,6 @@ namespace CodeInsight.Web.Controllers
             }
         });
         
-        private static IOption<NonEmptyString> GetVerifiedCode(string code, string state, ISession session)
-        {
-            var expectedState = session.Get<string>(CsrfSessionKey).GetOrElse("");
-            return NonEmptyString.Create(code).Where(_ => state == expectedState);
-        }
-
-        private Uri GetOAuthLoginUrl(string csrf)
-        {
-            return client.Oauth.GetGitHubLoginUrl(new OauthLoginRequest(configuration.ClientId)
-            {
-                Scopes = { "read:org", "repo" },
-                State = csrf
-            });
-        }
-        
-        private Task<string> GetOAuthAccessToken(NonEmptyString code)
-        {
-            var request = new OauthTokenRequest(configuration.ClientId, configuration.ClientSecret, code);
-            return client.Oauth.CreateAccessToken(request).Map(t => t.AccessToken);
-        }
-
-        private Task<IActionResult> ConnectionAction(Func<Connection, Task<IActionResult>> action)
-        {
-            var token = HttpContext.Session.Get<string>(ClientAuthenticator.GithubTokenSessionKey);
-            return token.Match(
-                t => action(new Connection(new Octokit.GraphQL.ProductHeaderValue(configuration.ApplicationName), t)),
-                _ => NotFound().Async<NotFoundResult, IActionResult>()
-            );
-        }
-
         private static ICompiledQuery<IEnumerable<List<RepositoryInputDto>>> GetAllRepositoriesQuery() =>
             new Query()
                 .Viewer
@@ -134,6 +133,16 @@ namespace CodeInsight.Web.Controllers
                     .ToList()
                 )
                 .Compile();
-
+        
+        #endregion
+        
+        private Task<IActionResult> ConnectionAction(Func<Connection, Task<IActionResult>> action)
+        {
+            var token = HttpContext.Session.Get<string>(ClientAuthenticator.GithubTokenSessionKey);
+            return token.Match(
+                t => action(new Connection(new Octokit.GraphQL.ProductHeaderValue(configuration.ApplicationName), t)),
+                _ => NotFound().Async<NotFoundResult, IActionResult>()
+            );
+        }
     }
 }
