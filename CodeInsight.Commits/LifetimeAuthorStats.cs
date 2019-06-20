884a8a804a06b32537b5a628bbe51e67cc4291a9
Lukáš Bastián <luk3bastian@gmail.com>
2019-06-20 09:26:00 +0200

    Added base for new features

---
+++ CodeInsight.Commits/LifetimeAuthorStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeInsight.Commits
+{
+    /*
+     * TABLE WITH STATS PER AUTHOR/CONTRIBUTOR
+     *
+     * Added/Deleted lines & difference
+     * Last commit & time spent on the project (first-last commit diff)
+     */
+    public class LifetimeAuthorStats
+    {
+    }
+}
---
+++ CodeInsight.Commits/OverTimeStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeInsight.Commits
+{
+    /*
+     * COMMIT STATS over (chosen) time
+     *
+     * Number of commits - graph with per week and per day per that week commit count
+     * Addition/Deletion graph
+     * ?? Change frequency
+     * ?? Table with per tasks changes - time of commit, task, added/deleted lines&diff, changed files, authors
+     */
+    class OverTimeStats
+    {
+    }
+}
